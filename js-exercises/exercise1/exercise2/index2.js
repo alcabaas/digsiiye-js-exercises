f275888addd32c33f845403f9e2aae909e3ecaa7
alcabaas <cabaasali4499@gmail.com>
2026-09-20 21:17:11 +0300

    exercise2

---
+++ js-exercises/exercise1/exercise2/index2.js
@@ -0,0 +1,18 @@
+let a = 15;
+let b = "15";
+
+console.log(a == b);
+
+console.log(a != b);
+
+console.log(a === b);
+
+console.log(a !== b);
+
+console.log(a > b);
+
+console.log(a < b);
+
+console.log(a >= b);
+
+console.log(a <= b);
--- lessons/index.js
+++ lessons/index.js
@@ -1,1 +1,30 @@
 console.log("my first jv code");
+
+let x = 10;
+
+let y = "10";
+
+// x iyo y waa isku mid (jawaabta= true)
+console.log(x == y);
+
+// x iyo y iskumid ma ahan (jawaabta= false)
+console.log(x != y);
+
+// x iyo y waa isku mid values ama qiimo ahaan
+// (jawaabta= false > sabto ah x waa number qiimo ahaan y ne waa qoraal caadi ah oo wax qiimo malahan)
+console.log(x === y);
+
+// x iyo y isku mid ma ahan value ahan ama qiimo ahaan (jawaabta= true > sabtoo ah x waa numbar y ne waa qoraal caadi ah malahan wax qiimo )
+console.log(x !== y);
+
+// x wexee kaweentahay y (jawaabta= false) sabto ah 10 iyo 10 makala weena markii qoraal ahan lo firiyo
+console.log(x > y);
+
+// x wee kayartahay y ( jawaabta= false ) sabatoo ah 10 iyo 10 wa isku mid markii qoral ahan loo firiyo
+console.log(x < y);
+
+// x we kawentahy ama wa isku mid y (jawaabta= true) ma isla eka lakin waa isku mid
+console.log(x >= y);
+
+// x wekayrtahy ama wa iskumid jawaabta= true
+console.log(x <= y);
