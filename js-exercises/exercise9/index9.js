23c33cc8ebb9d5f57dc0037b24b71ab01788f076
alcabaas <cabaasali4499@gmail.com>
2026-09-25 22:45:36 +0300

    exercise9

---
+++ js-exercises/exercise9/index9.js
@@ -0,0 +1,13 @@
+let library = [
+  {  title: "slaam",
+    author: "ghost",
+    year: 2018,
+  },
+  {
+    title: "mahyey",
+    author:"rbg",
+    year:2022
+  }
+]
+console.log(library[0].title)
+console.log(library[1].author)
